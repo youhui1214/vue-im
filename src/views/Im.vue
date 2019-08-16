4050491e82b6b781d61e316f3a307badfcbd3b6e
尤辉 <youhui@souche.com>
2019-08-16 15:48:19 +0800

    feat: 复制网易云信的代码

---
+++ src/views/Im.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <div>sdfsf</div>
+    </div>
+</template>
+
+<script>
+    import HelloWorld from '@/components/HelloWorld.vue'
+    
+    export default {
+        name: 'im',
+        components: {
+            HelloWorld
+        }
+    }
+</script>
